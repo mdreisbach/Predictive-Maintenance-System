b7fc09d8268a60b82ee6fb21d9d2fd7d71f73dbb
Miles Dreisbach <milesdreisbach@gmail.com>
2017-11-22 16:43:29 -0500

    Update Execute_Matlab.java

--- Execute_Matlab.java
+++ Execute_Matlab.java
@@ -5,23 +5,20 @@ import java.lang.ProcessBuilder;
 
 public class Execute_Matlab {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		try {
-			ProcessBuilder p = new ProcessBuilder();
-			p.command("C:\\PathToExe\\MyExe.exe");
+public Execute_Matlab(String File_Path) {
+	try {
+		ProcessBuilder p = new ProcessBuilder();
+		p.command(File_Path);
 			
-			System.out.println("Starting Matlab");
-			p.start();
+		System.out.println("Starting Matlab");
+		p.start();
 
-//			Process process = new ProcessBuilder("C:\\PathToExe\\MyExe.exe").start();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
+//		Process process = new ProcessBuilder("C:\\PathToExe\\MyExe.exe").start();
+			
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
 	}
+}
 	
 }
