5dadb56233af501f149bf47416e6cda3a0df5ced
Miles Dreisbach <milesdreisbach@gmail.com>
2017-12-09 17:13:25 -0500

    Add files via upload

--- SystemGUI.java
+++ SystemGUI.java
@@ -1,5 +1,7 @@
 package v1;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -16,8 +18,12 @@ import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.border.CompoundBorder;
 
 public class SystemGUI extends JFrame {
 
@@ -26,22 +32,43 @@ public class SystemGUI extends JFrame {
 		 	JRadioButton Normal_Button    = new JRadioButton("Normal");
 		 	JRadioButton Strict_Button = new JRadioButton("Strict");
 		 	JButton Confirm_Button = new JButton("Save/Okay");
+		 	
+		 	JRadioButton Yes_Button = new JRadioButton("Yes");
+		 	JRadioButton No_Button = new JRadioButton("No");
 
+		 	JLabel columnLabel = new JLabel("Column #:");
+		 	JTextField columnNum = new JTextField("",5);
+		 	
 		 	//Create a button group and add the buttons.
 		 	ButtonGroup bgroup = new ButtonGroup();
+		 	ButtonGroup cgroup = new ButtonGroup();
+		 	
 		 	bgroup.add(Low_Button);
 		 	bgroup.add(Normal_Button);
 		 	bgroup.add(Strict_Button);
-		 	bgroup.add(Confirm_Button);
-
+		 	
+		 	
+		 	cgroup.add(Yes_Button);
+		 	cgroup.add(No_Button);
+		 	cgroup.add(Confirm_Button);
+		 	
 		 	//Arrange buttons vertically in a panel
+		 	JFrame frame = new JFrame();
 		 	JPanel radioPanel = new JPanel();
+		 	JPanel discretePanel = new JPanel();
+		 	
 		 	radioPanel.setLayout(new GridLayout(4, 1));
 		 	radioPanel.add(Low_Button);
 		 	radioPanel.add(Normal_Button);
 		 	radioPanel.add(Strict_Button);
-		 	radioPanel.add(Confirm_Button);
 		 	
+		 	
+		 	discretePanel.add(Yes_Button);
+		 	discretePanel.add(No_Button);
+		 	No_Button.setSelected(true);
+		 	discretePanel.add(columnLabel);
+		 	discretePanel.add(columnNum);
+		 	discretePanel.add(Confirm_Button);
 		 	
 		 	try {
 				int setSelect = checkSetSelect();
@@ -66,25 +93,49 @@ public class SystemGUI extends JFrame {
 	        radioPanel.setBorder(BorderFactory.createTitledBorder(
 	                   BorderFactory.createEtchedBorder(), "Validation Strength"));
 	        
+	        discretePanel.setBorder(new CompoundBorder(
+	        	    BorderFactory.createMatteBorder(0, 0, 1, 0, Color.BLUE), 
+	        	    BorderFactory.createMatteBorder(0, 0, 1, 0, Color.RED)));
+	        
+	        discretePanel.setBorder(BorderFactory.createTitledBorder(
+	                   BorderFactory.createEtchedBorder(), "Has Discrete Column?"));
+	        
 		 	Confirm_Button.addActionListener(new ActionListener() {
 		 	    @Override
 		        public void actionPerformed(ActionEvent e) {
 		            if(Low_Button.isSelected()){
-		            	createTextFile(0);
+		            	createTextFile(0,columnNum.getText());
+		            	if(Yes_Button.isSelected()&&columnNum.getText().equals("")){
+		            		 JOptionPane.showMessageDialog(null, "Please enter a column number." , "WARNING", JOptionPane.INFORMATION_MESSAGE);
+		            	} else {
 		            	System.exit(0);
+		            	}
 		            } else if(Normal_Button.isSelected()){
-		            	createTextFile(1);
+		            	createTextFile(0,columnNum.getText());
+		            	if(Yes_Button.isSelected()&&columnNum.getText().equals("")){
+		            		 JOptionPane.showMessageDialog(null, "Please enter a column number." , "WARNING", JOptionPane.INFORMATION_MESSAGE);
+		            	} else {
 		            	System.exit(0);
+		            	}
 		            } else if(Strict_Button.isSelected()){
-		            	createTextFile(2);
+		            	createTextFile(0,columnNum.getText());
+		            	if(Yes_Button.isSelected()&&columnNum.getText().equals("")){
+		            		 JOptionPane.showMessageDialog(null, "Please enter a column number." , "WARNING", JOptionPane.INFORMATION_MESSAGE);
+		            	} else {
 		            	System.exit(0);
+		            	}
 		            }
 
 		        }
 		 	});
 		 	//... Set window attributes.
-		 	setContentPane(radioPanel);  // Button panel is only content.
-		 	pack();                      // Layout window.
+		 	frame.add(radioPanel, BorderLayout.NORTH);
+		 	frame.add(discretePanel, BorderLayout.SOUTH);
+		 
+//		 	setContentPane(radioPanel);  // Button panel is only content.
+//		 	setContentPane(discretePanel);
+		 	frame.pack();                      // Layout window.
+			frame.setVisible(true);
 	}
 
 	public int checkSetSelect() throws IOException {
@@ -110,20 +161,20 @@ public class SystemGUI extends JFrame {
 		return returnValue;
 	}
 	
-	public void createTextFile(int i) {
+	public void createTextFile(int i, String columnNum) {
 		try {
 			PrintWriter writer = new PrintWriter("the-file-name.txt", "UTF-8");
 			switch (i){
 				case 0:
-					writer.println("low");
+					writer.println("low" + "," + columnNum);
 					writer.close();
 					break;
 				case 1:
-					writer.println("normal");
+					writer.println("normal" + "," + columnNum);
 					writer.close();
 					break;
 				case 2: 
-					writer.println("strict");
+					writer.println("strict" + "," + columnNum);
 					writer.close();
 					break;
 			}
