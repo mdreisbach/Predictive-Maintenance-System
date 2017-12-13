32dfed3d464af1e8206426736b533f2f92883dd4
Miles Dreisbach <milesdreisbach@gmail.com>
2017-12-13 13:52:02 -0500

    Add files via upload

--- SystemGUI.java
+++ SystemGUI.java
@@ -2,6 +2,8 @@ package v1;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,6 +17,7 @@ import java.io.UnsupportedEncodingException;
 
 import javax.swing.AbstractButton;
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -32,6 +35,8 @@ public class SystemGUI extends JFrame {
 		 	JRadioButton Normal_Button    = new JRadioButton("Normal");
 		 	JRadioButton Strict_Button = new JRadioButton("Strict");
 		 	JButton Confirm_Button = new JButton("Save/Okay");
+		 	JButton Change_Button1 = new JButton("Change");
+		 	JButton Change_Button2 = new JButton("Change");
 		 	
 		 	JRadioButton Yes_Button = new JRadioButton("Yes");
 		 	JRadioButton No_Button = new JRadioButton("No");
@@ -39,23 +44,33 @@ public class SystemGUI extends JFrame {
 		 	JLabel columnLabel = new JLabel("Column #:");
 		 	JTextField columnNum = new JTextField("",5);
 		 	
+		 	JTextField dataLocationPath = new JTextField("example/file/path...");
+		 	JTextField modelExecutablePath = new JTextField("example/file/path/model.exe");
+		 	
 		 	//Create a button group and add the buttons.
 		 	ButtonGroup bgroup = new ButtonGroup();
 		 	ButtonGroup cgroup = new ButtonGroup();
+		 	ButtonGroup dgroup = new ButtonGroup();
+		 	ButtonGroup egroup = new ButtonGroup();
 		 	
 		 	bgroup.add(Low_Button);
 		 	bgroup.add(Normal_Button);
 		 	bgroup.add(Strict_Button);
 		 	
-		 	
 		 	cgroup.add(Yes_Button);
 		 	cgroup.add(No_Button);
 		 	cgroup.add(Confirm_Button);
+		 	
+		 	dgroup.add(Change_Button1);
+		 	egroup.add(Change_Button2);
 		 	
 		 	//Arrange buttons vertically in a panel
 		 	JFrame frame = new JFrame();
 		 	JPanel radioPanel = new JPanel();
 		 	JPanel discretePanel = new JPanel();
+		 	JPanel dataLocation = new JPanel();
+		 	JPanel modelExecutable = new JPanel();
+		 	
 		 	
 		 	radioPanel.setLayout(new GridLayout(4, 1));
 		 	radioPanel.add(Low_Button);
@@ -68,7 +83,16 @@ public class SystemGUI extends JFrame {
 		 	No_Button.setSelected(true);
 		 	discretePanel.add(columnLabel);
 		 	discretePanel.add(columnNum);
-		 	discretePanel.add(Confirm_Button);
+		 	//discretePanel.add(Confirm_Button);
+		 	
+		 	
+		 	dataLocation.add(dataLocationPath);
+		 	dataLocation.add(Change_Button1);
+		 	
+		 	
+		 	modelExecutable.add(modelExecutablePath);
+		 	modelExecutable.add(Change_Button2);
+		 	
 		 	
 		 	try {
 				int setSelect = checkSetSelect();
@@ -100,6 +124,13 @@ public class SystemGUI extends JFrame {
 	        discretePanel.setBorder(BorderFactory.createTitledBorder(
 	                   BorderFactory.createEtchedBorder(), "Has Discrete Column?"));
 	        
+	        dataLocation.setBorder(BorderFactory.createTitledBorder(
+	                   BorderFactory.createEtchedBorder(), "Training Data Location"));
+	        
+	        modelExecutable.setBorder(BorderFactory.createTitledBorder(
+	                   BorderFactory.createEtchedBorder(), "Trained Model Executable"));        
+	        
+	        
 		 	Confirm_Button.addActionListener(new ActionListener() {
 		 	    @Override
 		        public void actionPerformed(ActionEvent e) {
@@ -129,11 +160,28 @@ public class SystemGUI extends JFrame {
 		        }
 		 	});
 		 	//... Set window attributes.
-		 	frame.add(radioPanel, BorderLayout.NORTH);
-		 	frame.add(discretePanel, BorderLayout.SOUTH);
-		 
+		 	Container pane = null; 
+		 	pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+		 	
+		 	dataLocation.setAlignmentX(Component.CENTER_ALIGNMENT);
+	        pane.add(dataLocation);
+	        
+	        modelExecutable.setAlignmentX(Component.CENTER_ALIGNMENT);
+	        pane.add(modelExecutable);
+	        
+	        radioPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+	        pane.add(radioPanel);
+	        
+	        discretePanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+	        pane.add(discretePanel);
+	        
+	        Confirm_Button.setAlignmentX(Component.CENTER_ALIGNMENT);
+	        pane.add(Confirm_Button);
+		 	
 //		 	setContentPane(radioPanel);  // Button panel is only content.
 //		 	setContentPane(discretePanel);
+	        
+	        frame.add(pane);
 		 	frame.pack();                      // Layout window.
 			frame.setVisible(true);
 	}
@@ -160,6 +208,7 @@ public class SystemGUI extends JFrame {
 		}
 		return returnValue;
 	}
+	
 	
 	public void createTextFile(int i, String columnNum) {
 		try {
